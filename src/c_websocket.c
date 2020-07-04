5c574422fe3d7956d855369fadf038a22b6fde88
Niyamaka <niyamaka@yuneta.io>
2020-07-04 23:10:11 +0200

    mas trazas a tcp1, websocket close mejorado

--- src/c_websocket.c
+++ src/c_websocket.c
@@ -138,6 +138,7 @@ PRIVATE int frame_completed(hgobj gobj);
 PRIVATE sdata_desc_t tattr_desc[] = {
 /*-ATTR-type------------name----------------flag------------------------default---------description---------- */
 SDATA (ASN_BOOLEAN,     "connected",        SDF_RD,                     0,              ""),
+SDATA (ASN_INTEGER,     "timeout_close",    SDF_WR|SDF_PERSIST,    3*1000,              "Timeout to close"),
 SDATA (ASN_INTEGER,     "pingT",            SDF_WR|SDF_PERSIST,   50*1000,      "Ping interval. If value <= 0 then No ping"),
 SDATA (ASN_POINTER,     "tcp0",             0,                          0,              "Tcp0 connection"),
 SDATA (ASN_POINTER,     "user_data",        0,                          0,              "user data"),
@@ -541,7 +542,7 @@ PRIVATE void ws_close(hgobj gobj, int code, const char *reason)
             gobj_stop(priv->tcp0);
         }
     }
-    set_timeout(priv->timer, 1*1000);
+    set_timeout(priv->timer, gobj_read_int32_attr(gobj, "timeout_close"));
 }
 
 /***************************************************************************
