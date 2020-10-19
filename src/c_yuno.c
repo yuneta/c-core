722bfddfb51935ddc1459e00781da22481af1261
Niyamaka <niyamaka@yuneta.io>
2020-10-19 17:24:09 +0200

    enum fuera de type, danger

--- src/c_yuno.c
+++ src/c_yuno.c
@@ -131,7 +131,7 @@ PRIVATE json_t* cmd_remove_denied_ip(hgobj gobj, const char* cmd, json_t* kw, hg
 PRIVATE json_t* cmd_2key_get_schema(hgobj gobj, const char* cmd, json_t* kw, hgobj src);
 PRIVATE json_t* cmd_2key_get_value(hgobj gobj, const char* cmd, json_t* kw, hgobj src);
 PRIVATE json_t* cmd_2key_get_subvalue(hgobj gobj, const char* cmd, json_t* kw, hgobj src);
-PRIVATE json_t* cmd_topic_cols_desc(hgobj gobj, const char* cmd, json_t* kw, hgobj src);
+PRIVATE json_t* cmd_system_topic_schema(hgobj gobj, const char* cmd, json_t* kw, hgobj src);
 
 PRIVATE sdata_desc_t pm_help[] = {
 /*-PM----type-----------name------------flag------------default-----description---------- */
@@ -270,6 +270,7 @@ PRIVATE const char *a_help[] = {"h", "?", 0};
 PRIVATE const char *a_services[] = {"services", "list-services", 0};
 PRIVATE const char *a_read_attrs[] = {"read-attrs", 0};
 PRIVATE const char *a_read_attrs2[] = {"read-attrs2", 0};
+PRIVATE const char *a_sys_schema[] = {"topic-cols-schema", 0};
 
 PRIVATE sdata_desc_t command_table[] = {
 /*-CMD---type-----------name------------------------alias---items-----------json_fn----------------------description---------- */
@@ -348,7 +349,7 @@ SDATACM (ASN_SCHEMA,    "remove-denied-ip",         0,      pm_remove_denied_ip,
 SDATACM (ASN_SCHEMA,    "get-2key-schema",          0,      0, cmd_2key_get_schema, "Get 2key schema"),
 SDATACM (ASN_SCHEMA,    "get-2key-value",           0,      pm_2key_get_value, cmd_2key_get_value, "Get 2key value"),
 SDATACM (ASN_SCHEMA,    "get-2key-subvalue",        0,      pm_2key_get_value, cmd_2key_get_subvalue, "Get 2key sub-value"),
-SDATACM (ASN_SCHEMA,    "topic-cols-schema",        0,      0,  cmd_topic_cols_desc, "Get topic cols schema"),
+SDATACM (ASN_SCHEMA,    "system-topic-schema",      a_sys_schema, 0,  cmd_system_topic_schema, "Get system topic schema"),
 SDATA_END()
 };
 
@@ -3644,7 +3645,7 @@ PRIVATE json_t* cmd_2key_get_subvalue(hgobj gobj, const char* cmd, json_t* kw, h
 /***************************************************************************
  *
  ***************************************************************************/
-PRIVATE json_t* cmd_topic_cols_desc(hgobj gobj, const char* cmd, json_t* kw, hgobj src)
+PRIVATE json_t* cmd_system_topic_schema(hgobj gobj, const char* cmd, json_t* kw, hgobj src)
 {
     /*
      *  Inform
