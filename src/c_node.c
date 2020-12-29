caa5e797a524c646c5dabb773dafdcc172aa2529
Niyamaka <niyamaka@yuneta.io>
2020-12-29 18:14:35 +0100

    system gclass have not timers

--- src/c_node.c
+++ src/c_node.c
@@ -245,14 +245,6 @@ SDATA (ASN_POINTER,     "tranger",          SDF_RD|SDF_REQUIRED,0,              
 SDATA (ASN_OCTET_STR,   "treedb_name",      SDF_RD|SDF_REQUIRED,"",             "Treedb name"),
 SDATA (ASN_JSON,        "treedb_schema",    SDF_RD|SDF_REQUIRED,0,              "Treedb schema"),
 SDATA (ASN_INTEGER,     "exit_on_error",    0,                  LOG_OPT_EXIT_ZERO,"exit on error"),
-SDATA (ASN_INTEGER,     "timeout",          SDF_RD,             1*1000,         "Timeout"),
-SDATA (ASN_COUNTER64,   "txMsgs",           SDF_RD|SDF_PSTATS,  0,              "Messages transmitted"),
-SDATA (ASN_COUNTER64,   "rxMsgs",           SDF_RD|SDF_RSTATS,  0,              "Messages receiveds"),
-
-SDATA (ASN_COUNTER64,   "txMsgsec",         SDF_RD|SDF_RSTATS,  0,              "Messages by second"),
-SDATA (ASN_COUNTER64,   "rxMsgsec",         SDF_RD|SDF_RSTATS,  0,              "Messages by second"),
-SDATA (ASN_COUNTER64,   "maxtxMsgsec",      SDF_WR|SDF_RSTATS|SDF_AUTHZ_W, 0,   "Max Tx Messages by second"),
-SDATA (ASN_COUNTER64,   "maxrxMsgsec",      SDF_WR|SDF_RSTATS|SDF_AUTHZ_W, 0,   "Max Rx Messages by second"),
 SDATA (ASN_POINTER,     "user_data",        0,                  0,              "user data"),
 SDATA (ASN_POINTER,     "user_data2",       0,                  0,              "more user data"),
 SDATA (ASN_POINTER,     "subscriber",       0,                  0,              "subscriber of output-events. Not a child gobj."),
@@ -280,12 +272,6 @@ typedef struct _PRIVATE_DATA {
     json_t *treedb_schema;
     int32_t exit_on_error;
 
-    //int32_t timeout;
-    //hgobj timer;
-    uint64_t *ptxMsgs;
-    uint64_t *prxMsgs;
-    uint64_t txMsgsec;
-    uint64_t rxMsgsec;
 } PRIVATE_DATA;
 
 
@@ -304,10 +290,6 @@ typedef struct _PRIVATE_DATA {
 PRIVATE void mt_create(hgobj gobj)
 {
     PRIVATE_DATA *priv = gobj_priv_data(gobj);
-
-    //priv->timer = gobj_create(gobj_name(gobj), GCLASS_TIMER, 0, gobj);
-    priv->ptxMsgs = gobj_danger_attr_ptr(gobj, "txMsgs");
-    priv->prxMsgs = gobj_danger_attr_ptr(gobj, "rxMsgs");
 
     /*
      *  SERVICE subscription model
@@ -396,12 +378,6 @@ PRIVATE int mt_start(hgobj gobj)
         gobj
     );
 
-    /*
-     *  Periodic timer for tasks
-     */
-//     gobj_start(priv->timer);
-//     set_timeout_periodic(priv->timer, priv->timeout); // La verdadera
-
     return 0;
 }
 
@@ -411,9 +387,6 @@ PRIVATE int mt_start(hgobj gobj)
 PRIVATE int mt_stop(hgobj gobj)
 {
     PRIVATE_DATA *priv = gobj_priv_data(gobj);
-
-//     clear_timeout(priv->timer);
-//     gobj_stop(priv->timer);
 
     treedb_close_db(priv->tranger, priv->treedb_name);
 
@@ -2255,39 +2228,11 @@ PRIVATE int ac_treedb_update_node(hgobj gobj, const char *event, json_t *kw, hgo
 }
 
 /***************************************************************************
- *
- ***************************************************************************/
-PRIVATE int ac_timeout(hgobj gobj, const char *event, json_t *kw, hgobj src)
-{
-    PRIVATE_DATA *priv = gobj_priv_data(gobj);
-
-    uint64_t maxtxMsgsec = gobj_read_uint64_attr(gobj, "maxtxMsgsec");
-    uint64_t maxrxMsgsec = gobj_read_uint64_attr(gobj, "maxrxMsgsec");
-    if(priv->txMsgsec > maxtxMsgsec) {
-        gobj_write_uint64_attr(gobj, "maxtxMsgsec", priv->txMsgsec);
-    }
-    if(priv->rxMsgsec > maxrxMsgsec) {
-        gobj_write_uint64_attr(gobj, "maxrxMsgsec", priv->rxMsgsec);
-    }
-
-    gobj_write_uint64_attr(gobj, "txMsgsec", priv->txMsgsec);
-    gobj_write_uint64_attr(gobj, "rxMsgsec", priv->rxMsgsec);
-
-    priv->rxMsgsec = 0;
-    priv->txMsgsec = 0;
-
-    KW_DECREF(kw);
-    return 0;
-}
-
-/***************************************************************************
  *                          FSM
  ***************************************************************************/
 PRIVATE const EVENT input_events[] = { // HACK System gclass, not public events
     {"EV_TREEDB_UPDATE_NODE",   0,   0,    0},
     // bottom input
-    {"EV_TIMEOUT",              0,  0,  0},
-    {"EV_STOPPED",              0,  0,  0},
     {NULL, 0, 0, 0}
 };
 PRIVATE const EVENT output_events[] = { // HACK System gclass, not public events
@@ -2302,8 +2247,6 @@ PRIVATE const char *state_names[] = {
 
 PRIVATE EV_ACTION ST_IDLE[] = {
     {"EV_TREEDB_UPDATE_NODE",   ac_treedb_update_node,      0},
-    {"EV_TIMEOUT",              ac_timeout,                 0},
-    {"EV_STOPPED",              0,                          0},
     {0,0,0}
 };
 
