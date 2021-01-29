cadb984b649e47d93edf7efc0f78235c117a3f1d
Niyamaka <niyamaka@yuneta.io>
2021-01-29 09:44:52 +0100

    refactoring node options

--- src/c_node.c
+++ src/c_node.c
@@ -146,28 +146,28 @@ SDATA_END()
 PRIVATE sdata_desc_t pm_parents[] = {
 SDATAPM (ASN_OCTET_STR, "topic_name",   0,              0,          "Topic name"),
 SDATAPM (ASN_OCTET_STR, "node_id",      0,              0,          "Node id"),
-SDATAPM (ASN_OCTET_STR, "link",         0,              0,          "Link port to parents"),
-SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: only_id, list_dict, size"),
+SDATAPM (ASN_OCTET_STR, "link",         0,              0,          "Link port (fkey) to parents"),
+SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: refs, fkey_refs, only_id, fkey_only_id, list_dict, fkey_list_dict"),
 SDATA_END()
 };
 PRIVATE sdata_desc_t pm_childs[] = {
 SDATAPM (ASN_OCTET_STR, "topic_name",   0,              0,          "Topic name"),
 SDATAPM (ASN_OCTET_STR, "node_id",      0,              0,          "Node id"),
 SDATAPM (ASN_OCTET_STR, "hook",         0,              0,          "Hook port to childs"),
-SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: only_id, list_dict, size"),
+SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: refs, hook_refs, only_id, hook_only_id, list_dict, fkey_list_dict, size, hook_size"),
 SDATA_END()
 };
 PRIVATE sdata_desc_t pm_list_nodes[] = {
 /*-PM----type-----------name------------flag------------default-----description---------- */
 SDATAPM (ASN_OCTET_STR, "topic_name",   0,              0,          "Topic name"),
 SDATAPM (ASN_OCTET_STR, "filter",       0,              0,          "Search filter"),
-SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: only_id, list_dict, size"),
+SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: refs, hook_refs, fkey_refs, only_id, hook_only_id, fkey_only_id, list_dict, hook_list_dict, fkey_list_dict, size, hook_size"),
 SDATA_END()
 };
 PRIVATE sdata_desc_t pm_get_node[] = {
 SDATAPM (ASN_OCTET_STR, "topic_name",   0,              0,          "Topic name"),
 SDATAPM (ASN_OCTET_STR, "id",           0,              0,          "Node id"),
-SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: only_id, list_dict, size"),
+SDATAPM (ASN_JSON,      "options",      0,              0,          "Options: refs, hook_refs, fkey_refs, only_id, hook_only_id, fkey_only_id, list_dict, hook_list_dict, fkey_list_dict, size, hook_size"),
 SDATA_END()
 };
 PRIVATE sdata_desc_t pm_node_instances[] = {
