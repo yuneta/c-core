a274b3be969a6d2344b4519f0f3f0b7cb3db1d94
Niyamaka <niyamaka@yuneta.io>
2021-09-03 15:24:18 +0200

    v4.15.9 ycommand,ytests,ystats,ybatch with Oauth2 authenticacion; WARNING c_postgres change 'opened' by 'connected'; c_task change 'opened' by 'connected'

--- src/yuneta_version.h
+++ src/yuneta_version.h
@@ -12,7 +12,7 @@ extern "C"{
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __yuneta_version__  "4.15.8"
+#define __yuneta_version__  "4.15.9"
 #define __yuneta_date__     __DATE__ " " __TIME__
 
 #define __yuneta_long_version__ "yuneta:" __yuneta_version__ " " \
