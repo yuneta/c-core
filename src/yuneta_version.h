f4fa8e405ccc7061015ae8579eaf14e8148baea0
Niyamaka <niyamaka@yuneta.io>
2022-01-29 11:14:31 +0100

    v5.1.6

--- src/yuneta_version.h
+++ src/yuneta_version.h
@@ -12,7 +12,7 @@ extern "C"{
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __yuneta_version__  "5.1.5"     /* XX__yuneta_version__XX */
+#define __yuneta_version__  "5.1.6"     /* XX__yuneta_version__XX */
 #define __yuneta_date__     __DATE__ " " __TIME__
 
 #define __yuneta_long_version__ "yuneta:" __yuneta_version__ " " \
