7fef2fb56002aa98f8d8621b921768fab7d05343
ginsmar <ginsmar@gmail.com>
2022-08-22 20:20:40 +0200

    Version 5.12.2

--- src/yuneta_version.h
+++ src/yuneta_version.h
@@ -12,7 +12,7 @@ extern "C"{
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __yuneta_version__  "5.12.1"     /* XX__yuneta_version__XX */
+#define __yuneta_version__  "5.12.2"     /* XX__yuneta_version__XX */
 #define __yuneta_date__     __DATE__ " " __TIME__
 
 #define __yuneta_long_version__ "yuneta:" __yuneta_version__ " " \
