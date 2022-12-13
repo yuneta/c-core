624e36ee069b59e35f13c7db6e8120c47f345f7f
ginsmar <ginsmar@gmail.com>
2022-12-13 09:57:37 +0100

    version 6.3.1 change as_unique by as_service in ystats,ycommand,ytests,yuno_agent,yuno_cli en agent_secure_config/agent_insecure_config json configs

--- src/yuneta_version.h
+++ src/yuneta_version.h
@@ -12,7 +12,7 @@ extern "C"{
 /*********************************************************************
  *      Version
  *********************************************************************/
-#define __yuneta_version__  "6.3.0"     /* XX__yuneta_version__XX */
+#define __yuneta_version__  "6.3.1"     /* XX__yuneta_version__XX */
 #define __yuneta_date__     __DATE__ " " __TIME__
 
 #define __yuneta_long_version__ "yuneta:" __yuneta_version__ " " \
